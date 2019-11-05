47e5d2411c5d8796d247dc4c378fedbe76459f43
rktel <rakinakuy@hotmail.com>
2019-11-05 14:59:11 -0500

    change port ws 3

--- imports/ui/layouts/antapaccay/Antapaccay_Home.jsx
+++ imports/ui/layouts/antapaccay/Antapaccay_Home.jsx
@@ -9,7 +9,7 @@ import { Col } from 'rsuite'
 import { CheckPicker } from 'rsuite'
 import { DatePicker } from 'rsuite'
 
-const wsc = new WebSocket('ws://localhost:3000')
+const wsc = new WebSocket('ws://localhost:3001')
 
 const Home = () => {
     /* HELPS FUNCTION */
