44380ff957f89754d9281b8ba2a3ec1dd9eab5cc
rktel <rakinakuy@hotmail.com>
2019-11-06 12:07:29 -0500

    users

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from '../imports/ui/layouts/Login'
-import Home from '../imports/ui/layouts/Home'
 
 import Antapaccay_Home from '../imports/ui/layouts/antapaccay/Antapaccay_Home'
 import Pluton_Home from '../imports/ui/layouts/pluton/Pluton_Home'
@@ -11,6 +10,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Switch>
+                {console.log('Hello App')}
                 <Route path='/ntpccy' exact component={Antapaccay_Home} />
                 <Route path='/pltn' exact component={Pluton_Home} />
                 <Route path='/login' component={Login} />
