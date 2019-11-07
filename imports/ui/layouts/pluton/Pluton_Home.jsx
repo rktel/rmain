e0d764d62a00a166570e6e5a26d32eb25f560ae6
rktel <rakinakuy@hotmail.com>
2019-11-07 10:39:26 -0500

    form style api 13

--- imports/ui/layouts/pluton/Pluton_Home.jsx
+++ imports/ui/layouts/pluton/Pluton_Home.jsx
@@ -185,8 +185,8 @@ const Home = () => {
             </Header>
             <FlexboxGrid justify="start">
                 <FlexboxGrid.Item componentClass={Col} colspan={24} md={6}>
-                    <Panel bordered bodyFill>
-                        <Form fluid layout="horizontal" formValue={formValue} onChange={formValue => {
+                    <Panel bordered >
+                        <Form fluid formValue={formValue} onChange={formValue => {
                             console.log(formValue)
                             setFormValue(formValue)
                         }}>
