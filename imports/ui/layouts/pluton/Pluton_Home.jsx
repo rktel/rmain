34e0cfd69b7f66d8bb143034af30438f8296cd4f
rktel <rakinakuy@hotmail.com>
2019-11-08 10:55:43 -0500

    pltn table empty 14

--- imports/ui/layouts/pluton/Pluton_Home.jsx
+++ imports/ui/layouts/pluton/Pluton_Home.jsx
@@ -305,65 +305,7 @@ const Home = () => {
                 </FlexboxGrid.Item>
                 <FlexboxGrid.Item componentClass={Col} colspan={24} md={18}>
                     <Panel header="TABLA DE RESULTADOS" className="card" bordered>
-                        <Table
-                            height={400}
-                            data={resultList}
-                            onRowClick={el => { console.log(el); }}
-                            renderEmpty={() => <div className="flex-center">Aun sin datos</div>}
-                        >
-                            <Column width={80} fixed>
-                                <HeaderCell>Placa</HeaderCell>
-                                <Cell dataKey="Placa" />
-                            </Column>
-                            <Column width={72} >
-                                <HeaderCell>Tipo</HeaderCell>
-                                <Cell dataKey="Tipo" />
-                            </Column>
-                            <Column width={72} >
-                                <HeaderCell>gal</HeaderCell>
-                                <Cell dataKey="gal" />
-                            </Column>
-                            <Column width={72} >
-                                <HeaderCell>Precio(U)</HeaderCell>
-                                <Cell dataKey="Precio(U)" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Valor(V)</HeaderCell>
-                                <Cell dataKey="Valor(V)" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Precio(T)</HeaderCell>
-                                <Cell dataKey="Precio(T)" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Comodin</HeaderCell>
-                                <Cell dataKey="Comodin" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>km(R)</HeaderCell>
-                                <Cell dataKey="km(R)" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>gal(C)</HeaderCell>
-                                <Cell dataKey="gal(C)" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>km/gal</HeaderCell>
-                                <Cell dataKey="km/gal" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Dif. gal</HeaderCell>
-                                <Cell dataKey="Dif. gal" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Dscto. gal</HeaderCell>
-                                <Cell dataKey="Dscto. gal" />
-                            </Column>
-                            <Column width={80} >
-                                <HeaderCell>Dscto. (S/)</HeaderCell>
-                                <Cell dataKey="Dscto. (S/)" />
-                            </Column>
-                        </Table>
+
                     </Panel>
                 </FlexboxGrid.Item>
             </FlexboxGrid>
