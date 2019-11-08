aadecb8492767a8add4997db3a9d0b5a290c76f2
rktel <rakinakuy@hotmail.com>
2019-11-08 14:04:56 -0500

    pltn again 16

--- imports/ui/layouts/pluton/Pluton_Home.jsx
+++ imports/ui/layouts/pluton/Pluton_Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 import { Form, FormControl, ControlLabel, FormGroup, HelpBlock } from 'rsuite'
-import { InputPicker, DatePicker , InputNumber} from 'rsuite'
+import { InputPicker, DatePicker, InputNumber } from 'rsuite'
 import { Button } from 'rsuite'
-import { Container, Header, Content, Footer} from 'rsuite'
+import { Container, Header, Content, Footer } from 'rsuite'
+import { FlexboxGrid , Panel} from 'rsuite'
 
 const Home = () => {
     /* HELPS FUNCTION */
@@ -44,100 +45,113 @@ const Home = () => {
 
     return (<>
 
-    <Container>
-      <Header>Header</Header>
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
-    </Container>    
+        <Container className="flex-column-space-between">
+            <Header>Header</Header>
+            <Content>
+                <FlexboxGrid justify="start">
+                    <FlexboxGrid.Item componentClass={Col} colspan={24} md={6}>
+                        <Panel header="FORMULARIO" className="card" bordered >
+                            <Form formValue={formElements} onChange={handleOnChangeFormElements} style={{ height: 500, overflowY: 'scroll' }} fluid>
+                                <FormGroup>
+                                    <ControlLabel>Lista de unidades</ControlLabel>
+                                    <FormControl
+                                        name="inputPickerVehicles"
+                                        accepter={InputPicker}
+                                        data={plates}
+                                        placeholder="Seleccione unidad"
+                                    />
+                                    <HelpBlock>Solo una unidad seleccionable a la vez</HelpBlock>
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Fecha de inicio</ControlLabel>
+                                    <FormControl
+                                        name="datePickerStart"
+                                        accepter={DatePicker}
+                                        format="YYYY-MM-DD HH:mm:ss"
+                                        ranges={[]}
+                                    />
+                                    <HelpBlock>Presionar 'Ok' al finalizar</HelpBlock>
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Fecha de termino</ControlLabel>
+                                    <FormControl
+                                        name="datePickerEnd"
+                                        accepter={DatePicker}
+                                        format="YYYY-MM-DD HH:mm:ss"
+                                        ranges={[]}
+                                    />
+                                    <HelpBlock>Presionar 'Ok' al finalizar</HelpBlock>
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Tipo de combustible</ControlLabel>
+                                    <FormControl
+                                        name="inputPickerFuel"
+                                        accepter={InputPicker}
+                                        data={fuelList}
+                                        placeholder="Seleccione combustible"
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Galones</ControlLabel>
+                                    <FormControl
+                                        name="inputNumberGallons"
+                                        accepter={InputNumber}
+                                        step={0.01}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Precio unitario</ControlLabel>
+                                    <FormControl
+                                        name="inputNumberUnitaryPrice"
+                                        accepter={InputNumber}
+                                        step={0.01}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Valor venta</ControlLabel>
+                                    <FormControl
+                                        name="inputNumberSalesValue"
+                                        accepter={InputNumber}
+                                        step={0.01}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Precio total</ControlLabel>
+                                    <FormControl
+                                        name="inputNumberTotalPrice"
+                                        accepter={InputNumber}
+                                        step={0.01}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Factor comodin</ControlLabel>
+                                    <FormControl
+                                        name="inputNumberJoker"
+                                        accepter={InputNumber}
+                                        step={0.001}
+                                    />
+                                </FormGroup>
+                                <FormGroup>
+                                    <ControlLabel>Acciones</ControlLabel>
+                                    <section className="flex-row-space-between">
+                                        <Button appearance="primary" size="xs">Buscar</Button>
+                                        <Button appearance="default" size="xs">Limpiar</Button>
+                                    </section>
+                                </FormGroup>
+                            </Form>
+                        </Panel>
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item componentClass={Col} colspan={24} md={18}>
+                        <Panel header="TABLA DE RESULTADOS" className="card" bordered>
 
-        <Form formValue={formElements} onChange={handleOnChangeFormElements} fluid>
-            <FormGroup>
-                <ControlLabel>Lista de unidades</ControlLabel>
-                <FormControl
-                    name="inputPickerVehicles"
-                    accepter={InputPicker}
-                    data={plates}
-                    placeholder="Seleccione unidad"
-                />
-                <HelpBlock>Solo una unidad seleccionable a la vez</HelpBlock>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Fecha de inicio</ControlLabel>
-                <FormControl
-                    name="datePickerStart"
-                    accepter={DatePicker}
-                    format="YYYY-MM-DD HH:mm:ss"
-                    ranges={[]}
-                />
-                <HelpBlock>Presionar 'Ok' al finalizar</HelpBlock>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Fecha de termino</ControlLabel>
-                <FormControl
-                    name="datePickerEnd"
-                    accepter={DatePicker}
-                    format="YYYY-MM-DD HH:mm:ss"
-                    ranges={[]}
-                />
-                <HelpBlock>Presionar 'Ok' al finalizar</HelpBlock>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Tipo de combustible</ControlLabel>
-                <FormControl
-                    name="inputPickerFuel"
-                    accepter={InputPicker}
-                    data={fuelList}
-                    placeholder="Seleccione combustible"
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Galones</ControlLabel>
-                <FormControl
-                    name="inputNumberGallons"
-                    accepter={InputNumber}
-                    step={0.01}
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Precio unitario</ControlLabel>
-                <FormControl
-                    name="inputNumberUnitaryPrice"
-                    accepter={InputNumber}
-                    step={0.01}
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Valor venta</ControlLabel>
-                <FormControl
-                    name="inputNumberSalesValue"
-                    accepter={InputNumber}
-                    step={0.01}
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Precio total</ControlLabel>
-                <FormControl
-                    name="inputNumberTotalPrice"
-                    accepter={InputNumber}
-                    step={0.01}
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Factor comodin</ControlLabel>
-                <FormControl
-                    name="inputNumberJoker"
-                    accepter={InputNumber}
-                    step={0.001}
-                />
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Acciones</ControlLabel>
-                <section className="flex-row-space-between">
-                    <Button appearance="primary" size="xs">Buscar</Button>
-                    <Button appearance="default" size="xs">Limpiar</Button>
-                </section>
-            </FormGroup>
-        </Form>
+                        </Panel>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+            </Content>
+            <Footer>Footer</Footer>
+        </Container>
+
+
     </>)
 }
 
