4d0501106a7035e100a1a0ac715154fff6ba6e3e
rktel <rakinakuy@hotmail.com>
2019-11-08 11:20:08 -0500

    pltn again 2

--- imports/ui/layouts/pluton/Pluton_Home.jsx
+++ imports/ui/layouts/pluton/Pluton_Home.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react'
 
-import { Form, FormControl, ControlLabel, FormGroup } from 'rsuite'
+import { Form, FormControl, ControlLabel, FormGroup, HelpBlock } from 'rsuite'
 
 const Home = () => {
-    return(<>
-        <h1>Hello all</h1>
+    const [formElements, setFormElements] = useState({
+        inputTextName: 'Peppa pig'
+    })
+
+    return (<>
+        <Form formValue={formElements} onChange={
+            (elements)=>{
+                console.log('change', formElements)
+                setFormElements(elements)
+            }
+        }>
+            <FormGroup>
+                <ControlLabel>Texto de prueba</ControlLabel>
+                <FormControl
+                    name="inputTextName"
+                />
+                <HelpBlock>Mensaje de ayuda</HelpBlock>
+            </FormGroup>
+        </Form>
     </>)
 }
 
