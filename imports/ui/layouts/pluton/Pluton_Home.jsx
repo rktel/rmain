a9e9840296e073a69414f28b93495807a55b8cae
rktel <rakinakuy@hotmail.com>
2019-11-08 11:39:54 -0500

    pltn again 4

--- imports/ui/layouts/pluton/Pluton_Home.jsx
+++ imports/ui/layouts/pluton/Pluton_Home.jsx
@@ -1,23 +1,55 @@
 import React, { useEffect, useState } from 'react'
 
 import { Form, FormControl, ControlLabel, FormGroup, HelpBlock } from 'rsuite'
+import { CheckPicker } from 'rsuite'
 
 const Home = () => {
-    const [formElements, setFormElements] = useState({
-        inputTextName: 'Peppa pig'
-    })
+    /* HELPS FUNCTION */
+    const defaultDateStart = () => {
+        let xdate = new Date();
+        xdate.setHours(0, 0, 0, 0)
+        return xdate
+    }
+    const defaultDateEnd = () => {
+        let ydate = new Date();
+        ydate.setHours(23, 59, 59, 999)
+        return ydate
+    }
+    /* HELPS DATA */
+    const fuelList = [
+        { label: 'D2', value: 'D2' },
+        { label: 'G-90', value: 'G-90' },
+        { label: 'G-95', value: 'G-95' },
+        { label: 'GLP', value: 'GLP' },
+    ]
+    /* HOOKS */
+    useEffect(() => { Meteor.call('Pluton_plates', (error, elements) => setPlates(elements)) }, [])
+    
+    const [plates, setPlates] = useState([])
 
+    const [formElements, setFormElements] = useState({
+        checkPickerVehicles: [],
+        datePickerStart: defaultDateStart(),
+        datePickerEnd: defaultDateEnd(),
+        inputPickerFuel: 'Peppa',
+        inputNumberGallons: 0.01,
+        inputNumberUnitaryPrice: 0.01,
+        inputNumberSalesValue: 0.01,
+        inputNumberTotalPrice: 0.01,
+        inputNumberJoker: 0.025
+    })
+    const handleOnChangeFormElements = elements => {
+        setFormElements(elements)
+        console.log(formElements)
+    }
     return (<>
-        <Form formValue={formElements} onChange={
-            (elements)=>{
-                setFormElements(elements)
-                console.log('change', formElements)
-            }
-        }>
+        <Form formValue={formElements} onChange={handleOnChangeFormElements}>
             <FormGroup>
                 <ControlLabel>Texto de prueba</ControlLabel>
                 <FormControl
-                    name="inputTextName"
+                    name="checkPickerVehicles"
+                    accepter={CheckPicker}
+                    data={plates}
                 />
                 <HelpBlock tooltip>Mensaje de ayuda</HelpBlock>
             </FormGroup>
