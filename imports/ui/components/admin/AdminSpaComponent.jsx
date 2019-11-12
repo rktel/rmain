4f85fda599c8cd3029f82224fd429da07000f4d3
rktel <rakinakuy@hotmail.com>
2019-11-12 14:28:32 -0500

    Admin_Home 31

--- imports/ui/components/admin/AdminSpaComponent.jsx
+++ imports/ui/components/admin/AdminSpaComponent.jsx
@@ -5,7 +5,7 @@ import { FlexboxGrid, Panel, Col } from 'rsuite'
 const AdminSpaComponent = (props) => {
     return (
         <Panel bordered bodyFill style={{height: props.height, overflowY: 'scroll'}}>
-            <h1>A</h1>
+            <h1>Spa</h1>
             <h1>A</h1>
             <h1>A</h1>
             <h1>A</h1>
--- imports/ui/components/admin/AdminUsersComponent.jsx
+++ imports/ui/components/admin/AdminUsersComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FlexboxGrid, Panel, Col } from 'rsuite'
 
 const AdminUsersComponent = (props) => {
     return (
         <Panel bordered bodyFill style={{ height: props.height, overflowY: 'scroll' }}>
-            <h1>A</h1>
+            <h1>Users</h1>
             <h1>A</h1>
             <h1>A</h1>
             <h1>A</h1>
