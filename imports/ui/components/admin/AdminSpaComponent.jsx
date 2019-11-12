b0e422a829764b81c4884829f601ec3121167e4d
rktel <rakinakuy@hotmail.com>
2019-11-12 14:32:10 -0500

    Admin_Home 32

--- imports/ui/components/admin/AdminSpaComponent.jsx
+++ imports/ui/components/admin/AdminSpaComponent.jsx
@@ -4,17 +4,11 @@ import { FlexboxGrid, Panel, Col } from 'rsuite'
 
 const AdminSpaComponent = (props) => {
     return (
-        <Panel bordered bodyFill style={{height: props.height, overflowY: 'scroll'}}>
-            <h1>Spa</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
-            <h1>A</h1>
+        <Panel bordered bodyFill style={{ height: props.height, overflowY: 'scroll' }}>
+            <Nav>
+                <Nav.Item href={'/ntpccy'}>Antapaccay</Nav.Item>
+                <Nav.Item href={'/pltn'}>Pluton</Nav.Item>
+            </Nav>
         </Panel>
     )
 }
