287af1a8021da18be3b6506c85258e813e8b14eb
rktel <rakinakuy@hotmail.com>
2019-11-12 09:35:19 -0500

    add Login logic 6

--- imports/ui/layouts/admin/Admin_Home.jsx
+++ imports/ui/layouts/admin/Admin_Home.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { Container, Header, Footer } from 'rsuite'
 
-const Home = (props) =>{
-
-    return(
+const Home = (props) => {
+    const handleClickLogoutBtn = () => {
+        Meteor.logout()
+        resetRmainUser()
+        props.history.push('/login')
+    }
+    const resetRmainUser = () => {
+        localStorage.removeItem('rmain_user_role')
+        localStorage.removeItem('rmain_user_spa')
+        localStorage.removeItem('rmain_user_firstname')
+        localStorage.removeItem('rmain_user_lastname')
+    }
+    return (
         <>
-            <h1>ADMIN</h1>
+            <Container>
+                <Header>
+                    <Navbar appearance="inverse">
+                        <Navbar.Header>
+                            <img src="/img/seclog.png" alt="Securitas Logo" height="45" className="navbar-brand" />
+                        </Navbar.Header>
+                        <Navbar.Body>
+                            <Nav pullRight>
+                                <Dropdown title={localStorage.getItem('rmain_user_firstname') + ' ' + localStorage.getItem('rmain_user_lastname')} icon={<Icon icon="user-circle-o" />}>
+                                    <Dropdown.Item onClick={handleClickLogoutBtn}>Cerrar sesión</Dropdown.Item>
+                                </Dropdown>
+                            </Nav>
+                        </Navbar.Body>
+                    </Navbar>
+                </Header>
+                <section>Section</section>
+                <Footer>Footer</Footer>
+            </Container>
         </>
     )
 
