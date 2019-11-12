71821e0f0291e213fb61d52f92e36410f2a48591
rktel <rakinakuy@hotmail.com>
2019-11-12 16:03:30 -0500

    Admin_Home 37

--- imports/ui/components/admin/AdminUsersComponent.jsx
+++ imports/ui/components/admin/AdminUsersComponent.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
-import { Panel, ButtonToolbar, IconButton, Icon } from 'rsuite'
+import { Panel, ButtonToolbar, IconButton, Icon, Divider } from 'rsuite'
+import { Table } from 'rsuite'
+const { Column, HeaderCell, Cell } = Table
+
+const users= [
+    {
+        firstname: 'George',
+        username: 'gpig'
+    }
+]
 
 const AdminUsersComponent = (props) => {
     return (
@@ -9,6 +18,40 @@ const AdminUsersComponent = (props) => {
                     Nuevo
                 </IconButton>
             </ButtonToolbar>
+            <Divider />
+            <Table
+                height={'100%'}
+                data={users}
+                onRowClick={data => {
+                    console.log(data);
+                }}
+            >
+                <Column width={70} align="center" fixed>
+                    <HeaderCell>Nombre</HeaderCell>
+                    <Cell dataKey="firstname" />
+                </Column>
+                <Column width={300}>
+                    <HeaderCell>Usuario</HeaderCell>
+                    <Cell dataKey="username" />
+                </Column>
+                <Column width={120} fixed="right">
+                    <HeaderCell>Action</HeaderCell>
+
+                    <Cell>
+                        {rowData => {
+                            function handleAction() {
+                                alert(`id:${rowData}`)
+                            }
+                            return (
+                                <span>
+                                    <a onClick={handleAction}> Edit </a> |{' '}
+                                    <a onClick={handleAction}> Remove </a>
+                                </span>
+                            )
+                        }}
+                    </Cell>
+                </Column>
+            </Table>
         </Panel>
     )
 }
