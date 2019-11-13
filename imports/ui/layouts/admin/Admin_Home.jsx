ba6e37d8787a6e2d9bfd76992bf405c1beffa9bd
rktel <rakinakuy@hotmail.com>
2019-11-13 09:02:19 -0500

    Admin_Home Users 4

--- imports/ui/layouts/admin/Admin_Home.jsx
+++ imports/ui/layouts/admin/Admin_Home.jsx
@@ -45,7 +45,7 @@ const Home = (props) => {
                         </Navbar.Body>
                     </Navbar>
                 </Header>
-                <section style={{ height: contentHeight }} >
+                <section  >
                     <FlexboxGrid style={{ marginTop: 8 }}>
                         <FlexboxGrid.Item componentClass={Col} colspan={24} md={4}>
                             <Panel bordered bodyFill style={{height: contentHeight - 8}}>
