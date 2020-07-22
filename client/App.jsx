f2059b5e381037647a322c9a7950fcb35125880d
rktel <rakinakuy@hotmail.com>
2020-07-21 19:22:57 -0500

    add Esmeralda 2

--- client/App.jsx
+++ client/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
                 <Route path='/pltn' render={_ => (
                     (Meteor.userId() && localStorage.getItem('rmain_user_spa') == 'Pluton' && localStorage.getItem('rmain_user_role') == 'Tecnico' || localStorage.getItem('rmain_user_role') == 'Admin' || localStorage.getItem('rmain_user_role') == 'Hyperadmin') ? (<Pluton_Home />) : (<Redirect to='/login' />)
                 )} />
-                <Route path='/pltn' render={_ => (
+                <Route path='/smrld' render={_ => (
                     (Meteor.userId() && localStorage.getItem('rmain_user_spa') == 'Esmeralda' && localStorage.getItem('rmain_user_role') == 'Tecnico' || localStorage.getItem('rmain_user_role') == 'Admin' || localStorage.getItem('rmain_user_role') == 'Hyperadmin') ? (<Esmeralda_Home />) : (<Redirect to='/login' />)
                 )} />
                 <Route path='/admin' render={_=>(
